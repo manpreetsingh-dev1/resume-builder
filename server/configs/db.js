66ea56cc9a0f0183117863dae3fd2e41677f14c6
manpreetsingh-dev1 <manpreetsinghv01@gmail.com>
2026-03-25 17:46:36 +0530

    fix build errors final

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  mongoose.connection.on("connected", () => {
+    console.log("database connected successfully");
+  });
+
+  if (!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI environment variable not set");
+  }
+
   try {
-    mongoose.connection.on("connected", () => {
-      console.log("database connected successfully");
-    });
-
-    if (!process.env.MONGODB_URI) {
-      throw new Error("MONGODB_URI environment variable not set");
-    }
-
     await mongoose.connect(process.env.MONGODB_URI);
-
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
+    throw error;
   }
 };
 
